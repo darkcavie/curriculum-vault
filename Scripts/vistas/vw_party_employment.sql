79f5d4af5f674f3bbe84c762560ce20eb75c065a
Adrian Vega Gomez <darkcavie@gmail.com>
2024-12-26 15:13:26 +0100

    Creada vista que recupera los enlaces de tiempo

--- Scripts/vistas/vw_party_employment.sql
+++ Scripts/vistas/vw_party_employment.sql
@@ -1,17 +1,33 @@
 create or replace view vault.vw_party_employment as
+with satelite as (
+	select
+		lnk_party_employment_key,
+		starts,
+		ends,
+		rank() over (
+			partition by 
+				lnk_party_employment_key,
+				starts,
+				ends
+			order by 
+				sat_load_dts desc
+		) as ranked
+	from vault.sat_party_employment_period
+)
 select
 	company.hub_party_key as company_key,
 	company.party_name as company_name,
 	employee.hub_party_key as employee_name,
 	employee.party_name as employee_key, 
 	period.starts, 
-	period.ends,
-	period.sat_load_dts
-from  vault.sat_party_employment_period period
+	period.ends
+from satelite as period
 inner join vault.lnk_party_employment lpe on 
 	period.lnk_party_employment_key = lpe.lnk_party_employment_key
 inner join vault.hub_party company on 
 	company.hub_party_key = lpe.hub_party_key_company
 inner join vault.hub_party employee on 
-	employee.hub_party_key = lpe.hub_party_key_employee;
+	employee.hub_party_key = lpe.hub_party_key_employee
+where 
+	period.ranked = 1;
 
